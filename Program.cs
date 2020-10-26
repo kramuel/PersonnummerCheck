7b6be22a5895898a594dbc7626246657ec37435a
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-26 14:54:05 +0100

    comment process

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace PersonnummerCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //welcome menu
+
+
+            //take input
+
+
+            //check valid year
+
+
+            //check valid month
+
+
+            //check valid day
+
+
+            //check valid last numbers (man||woman)
+
+
+            //return if whole number is valid and if man or woman
+
+            // ask for nother number(loop?)
+
+
+
+
+
+
         }
     }
 }
