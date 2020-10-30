ed16000ce4dde47a81adb1424b176ad20da307e9
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-30 17:52:11 +0100

    GetUserInput's errohandling revised and optimised can now take both formats as inputs Updated ValidDateCheck, removed unnecessary boolean

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace PersonnummerCheck
 {
@@ -9,38 +10,43 @@ namespace PersonnummerCheck
             //variables 
             string personalNumber;
             int year, month, day, birthNumber;
+            string plusOrMinus;
 
             //menu loop
             while (true)
             {
-
                 //welcome menu
                 PrintMenu();
 
-                //take input and check if numbers
-                personalNumber = GetUserInput();
+                //take input and check if characters are numbers. outputs plusOrMinus sign
+                personalNumber = GetUserInput(out plusOrMinus);
 
                 //exit loop(program)
                 if (personalNumber == "0")
                     break;
 
                 //splits personalnumber into substrings and parses to integers
-                SplitPersonalNumber(personalNumber, out year, out month, out day, out birthNumber);
+                SplitPersonalNumber(personalNumber, plusOrMinus, out year, out month, out day, out birthNumber);
 
                 //checks if year, month and day is Valid
                 if (ValidDateCheck(year, month, day))
                 {
-                    Console.WriteLine("The Personalnumber VALIDATED, CORRECT");
+                    Console.WriteLine("Detta personnummer är VALIDERAT/GILTIGT OCH KORREKT");
                     //check valid last numbers (man||woman)  3 numbers can only be 000-999 no check needed
                     if (CheckIfWoman(birthNumber))
-                        Console.WriteLine("This persons legal gender is Woman");
+                    {
+                        Console.WriteLine("Det juridiska könet är KVINNA");
+                    }
                     else
-                        Console.WriteLine("This persons legal gender is Man");
+                    {
+                        Console.WriteLine("Det juridiska könet är MAN");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("The Personalnumber IS NOT VALIDATED");
+                    Console.WriteLine("Detta personnummer är ICKE GILTIGT");
                 }
+
 
                 //pause
                 Console.WriteLine("Press any key to try again.");
@@ -65,33 +71,50 @@ namespace PersonnummerCheck
 
         /// <summary>
         /// Takes users input and returns a validated string. Loops if bad string is entered.
-        /// Input string has to be 12 characters.
+        /// Input string has to be numbers with these formats: YYYYMMDDnnnc || YYMMDD-nnnc
         /// </summary>
         /// <returns></returns>
-        static string GetUserInput()
+        static string GetUserInput(out string plusOrMinus)
         {
-            long intUserInput;
             string userInput = Console.ReadLine();
+            plusOrMinus = "";
+            bool punctMarkOK = true;
 
-            //adding better error handling later :)
-            //checks if the string consists of numbers
-            while (!long.TryParse(userInput, out intUserInput))
+            //wrong input loop
+            while (true)
             {
-                Console.WriteLine("Incorrect input, please try again: ");
+                if (userInput.Length == 12 || userInput.Length == 11 || userInput == "0")//check size
+                {
+                    if (userInput.Length == 11)//if length is 11 = format is YYMMDD-nnnc
+                    {
+                        //removs plusOrMinus sign so string can be parsed. outputs plusOrMinus
+                        if (userInput[6] == '+' || userInput[6] == '-')
+                        {
+                            plusOrMinus = userInput.Substring(6, 1);
+                            userInput = userInput.Remove(6, 1);
+                            punctMarkOK = true;
+                        }
+                        else//if anyhting other than '+' or '-', this bool prevents the break from loop
+                        {
+                            punctMarkOK = false;
+                        }
+                    }
+                    //checks if all characters are digits ( and if the punctuation mark was not correct )
+                    if (long.TryParse(userInput, out _) && punctMarkOK)
+                    {
+                        break; // succesful
+                    }
+                }
+
+                Console.WriteLine("Felaktig inmatning, försök igen: ");
                 userInput = Console.ReadLine();
             }
+
 
             if (userInput == "0")
                 return "0";
-
-            // add "or 10" digits later
-            while (userInput.Length != 12)
-            {
-                Console.WriteLine("Incorrect input, please try again: ");
-                userInput = Console.ReadLine();
-            }
-
-            return userInput;
+            else
+                return userInput;
         }
 
         /// <summary>
@@ -102,13 +125,40 @@ namespace PersonnummerCheck
         /// <param name="month"></param>
         /// <param name="day"></param>
         /// <param name="birthNumber"></param>
-        static void SplitPersonalNumber(string personalNumber, out int year, out int month, out int day, out int birthNumber)
+        static void SplitPersonalNumber(string personalNumber, string plusOrMinus, out int year, out int month, out int day, out int birthNumber)
         {
             //YYYYMMDDnnnc
-            year = int.Parse(personalNumber.Substring(0, 4));
-            month = int.Parse(personalNumber.Substring(4, 2));
-            day = int.Parse(personalNumber.Substring(6, 2));
-            birthNumber = int.Parse(personalNumber.Substring(8, 3));
+            if (personalNumber.Length == 12)
+            {
+                year = int.Parse(personalNumber.Substring(0, 4));
+                month = int.Parse(personalNumber.Substring(4, 2));
+                day = int.Parse(personalNumber.Substring(6, 2));
+                birthNumber = int.Parse(personalNumber.Substring(8, 3));
+            }
+            //YYMMDD-nnnc || YYMMDD+nnnc
+            else if (personalNumber.Length == 10)
+            {
+                year = int.Parse(personalNumber.Substring(0, 2));
+                month = int.Parse(personalNumber.Substring(2, 2));
+                day = int.Parse(personalNumber.Substring(4, 2));
+                birthNumber = int.Parse(personalNumber.Substring(6, 3));
+
+                if (plusOrMinus == "+")
+                {
+                    year += 1900;
+                }
+                else if (plusOrMinus == "-")
+                {
+                    year += 2000;
+                }
+            }
+            else
+            {
+                year = 0;
+                month = 0;
+                day = 0;
+                birthNumber = 0;
+            }
         }
 
         /// <summary>
@@ -118,17 +168,9 @@ namespace PersonnummerCheck
         /// <returns></returns>
         static bool ValidDateCheck(int year, int month, int day)
         {
-            bool leapYear;
-
-            //checks if year is valid and if leap year
-            if (year >= 1753 && year <= 2020)
-            { 
-                leapYear = LeapYearCheck(year);
-            }
-            else
-            {
+            //return false if year is not in specified range ( 1753 - 2020 )
+            if (! ( year >= 1753 && year <= 2020 ) )
                 return false;
-            }
 
             //day can be between 1 and 31 in these months (jan,mar,may,jul,aug,oct,dec)
             if ((day >= 1 && day <= 31) && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
@@ -143,15 +185,14 @@ namespace PersonnummerCheck
             {
                 return true;
             }//day can be 29 in feb if leapyear
-            else if (day == 29 && month == 2 && leapYear)
+            else if (day == 29 && month == 2 && LeapYearCheck(year))
             {
                 return true;
             }
-            else
+            else//all other months and days returns false
             {
                 return false;
             }
-
         }
 
         /// <summary>
@@ -173,10 +214,32 @@ namespace PersonnummerCheck
         static bool CheckIfWoman(int birthNumber)
         {
             //returns true if birthNumber is even = it is a woman
-            if (birthNumber % 2 == 0)
-                return true;
-            else
-                return false;
+            return birthNumber % 2 == 0;
         }
+
+        ///// <summary>
+        ///// Checks if input was in format YYYY or YY, adds 1900or2000 depending on input
+        ///// Returns true if year is in valid range ( 1753 - 2020 )
+        ///// </summary>
+        ///// <param name="year"></param>
+        ///// <param name="plusOrMinus"></param>
+        ///// <returns></returns>
+        //static bool ValidYearCheck(ref int year, string plusOrMinus)
+        //{
+        //    if (plusOrMinus == "+")
+        //    {
+        //        year += 1900;
+        //    }
+        //    else if (plusOrMinus == "-")
+        //    {
+        //        year += 2000;
+        //    }
+
+        //    //returns true if year is 1753-2020
+        //    if (year >= 1753 && year <= 2020)
+        //        return true;
+        //    else
+        //        return false;
+        //}
     }
 }
